541626337a689d987673b2446807aa9fcaf4be72
Diego <dpardoblacutt@gmail.com>
2023-05-11 19:07:19 -0400

    Create hardcoded database of 12 beers

---
+++ backend/app/db/database.py
@@ -0,0 +1,74 @@
+beers = [
+    {
+        "name": "Budweiser",
+        "type": "Lager",
+        "alcohol_by_volume": 5.0,
+        "brewery_name": "Anheuser-Busch",
+    },
+    {
+        "name": "Heineken",
+        "type": "Lager",
+        "alcohol_by_volume": 5.0,
+        "brewery_name": "Heineken N.V.",
+    },
+    {
+        "name": "Corona Extra",
+        "type": "Pale Lager",
+        "alcohol_by_volume": 4.6,
+        "brewery_name": "Cervecería Modelo",
+    },
+    {
+        "name": "Guinness",
+        "type": "Stout",
+        "alcohol_by_volume": 4.2,
+        "brewery_name": "St. James's Gate Brewery",
+    },
+    {
+        "name": "Pilsner Urquell",
+        "type": "Pilsner",
+        "alcohol_by_volume": 4.4,
+        "brewery_name": "Plzeňský Prazdroj",
+    },
+    {
+        "name": "Stella Artois",
+        "type": "Pilsner",
+        "alcohol_by_volume": 5.0,
+        "brewery_name": "Stella Artois Brewery",
+    },
+    {
+        "name": "Blue Moon",
+        "type": "Witbier",
+        "alcohol_by_volume": 5.4,
+        "brewery_name": "Blue Moon Brewing Co.",
+    },
+    {
+        "name": "Hoegaarden",
+        "type": "Witbier",
+        "alcohol_by_volume": 4.9,
+        "brewery_name": "Hoegaarden Brewery",
+    },
+    {
+        "name": "Samuel Adams Boston Lager",
+        "type": "Lager",
+        "alcohol_by_volume": 5.0,
+        "brewery_name": "Boston Beer Company",
+    },
+    {
+        "name": "Sierra Nevada Pale Ale",
+        "type": "Pale Ale",
+        "alcohol_by_volume": 5.6,
+        "brewery_name": "Sierra Nevada Brewing Co.",
+    },
+    {
+        "name": "Coors Light",
+        "type": "Light Lager",
+        "alcohol_by_volume": 4.2,
+        "brewery_name": "Coors Brewing Company",
+    },
+    {
+        "name": "Beck's",
+        "type": "Pilsner",
+        "alcohol_by_volume": 5.0,
+        "brewery_name": "Beck's Brewery",
+    },
+]
