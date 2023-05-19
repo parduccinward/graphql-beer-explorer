90b7b037a45767527372738139a5c27bc50ec611
Diego <dpardoblacutt@gmail.com>
2023-05-19 16:05:56 -0400

    Rework Searchbar button for handleclick fetching beers

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,26 +1,22 @@
-import * as React from 'react';
-import { Box } from '@mui/material';
-import SearchBar from './SearchBar';
+import * as React from "react";
+import { Box } from "@mui/material";
+import SearchBar from "./SearchBar";
 
 const Main: React.FC = () => {
-  const handleSearchChange = (searchText: string) => {
-    console.log(searchText);
-  };
-
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        gap: 2
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: 2,
       }}
     >
       <h1>Beer explorer</h1>
       <p>Use the search bar below to search your favorite drinks.</p>
-      <SearchBar onSearchChange={handleSearchChange} />
+      <SearchBar />
     </Box>
   );
 };
--- frontend/src/SearchBar.tsx
+++ frontend/src/SearchBar.tsx
@@ -1,29 +1,31 @@
-import * as React from 'react';
-import { Box, TextField, IconButton } from '@mui/material';
-import { Search } from '@mui/icons-material';
+import * as React from "react";
+import { Box, TextField, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { fetchBeer } from "./api/FetchBeers";
 
-interface SearchBarProps {
-  onSearchChange: (searchText: string) => void;
-}
+const SearchBar: React.FC = ({}) => {
+  const [searchText, setSearchText] = React.useState("");
 
-const SearchBar: React.FC<SearchBarProps> = ({ onSearchChange }) => {
-  const [searchText, setSearchText] = React.useState('');
-
-  const handleSearchTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchTextChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchText(event.target.value);
-    onSearchChange(event.target.value);
+  };
+
+  const handleClick = async () => {
+    fetchBeer(searchText);
   };
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <TextField 
-        value={searchText} 
-        onChange={handleSearchTextChange} 
-        variant="outlined" 
-        size="small" 
-        placeholder="Search..." 
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <TextField
+        value={searchText}
+        onChange={handleSearchTextChange}
+        variant="outlined"
+        size="small"
+        placeholder="Search..."
       />
-      <IconButton>
+      <IconButton onClick={handleClick}>
         <Search />
       </IconButton>
     </Box>
