9426634177ba40fb9b78da0b96a857abf25c4d24
Diego <dpardoblacutt@gmail.com>
2023-05-19 17:49:03 -0400

    Create beer table

---
+++ frontend/src/BeerTable.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { BeerTableProps } from "./types/beers";
+
+const BeerTable: React.FC<BeerTableProps> = ({ beers }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Alcohol By Volume</TableCell>
+            <TableCell>Brewery Name</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {beers.map((beer, index) => (
+            <TableRow key={index}>
+              <TableCell>{beer.name}</TableCell>
+              <TableCell>{beer.type}</TableCell>
+              <TableCell>{beer.alcoholByVolume}</TableCell>
+              <TableCell>{beer.breweryName}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default BeerTable;
