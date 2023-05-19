b1870e45f32a7d31a049064d3ea71064dfcef280
Diego <dpardoblacutt@gmail.com>
2023-05-19 16:19:14 -0400

    Add CORS to flask server

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -1,14 +1,17 @@
-from app.main.schema import schema
 from flask import Blueprint
+from flask_cors import CORS
 from flask_graphql import GraphQLView
+from app.main.schema import schema
+import os
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
+CORS(main, resources={r"/graphql": {"origins": os.getenv("CORS_ORIGIN")}})
 
 main.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+    "/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
 )
 
-@main.route('/')
+
+@main.route("/")
 def homepage():
     return "This is the homepage for the beer explorer"
